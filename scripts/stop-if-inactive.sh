02d2582d67a4c5ee9b46425f5c1aee1512a3b565
GrahamOHagan <64635731+GrahamOHagan@users.noreply.github.com>
2023-05-25 11:19:27 +0100

    Add modified cloud9 stop-if-inactive.sh script
    This is an alternate script that replaces ~/.c9/stop-if-inactive.sh on the Cloud9 instance. It should work for VSCode & Intellij IDEA in adition to the Cloud9 IDE.

---
+++ scripts/stop-if-inactive.sh
@@ -0,0 +1,63 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+#!/bin/bash
+set -euo pipefail
+CONFIG=$(cat /home/ec2-user/.c9/autoshutdown-configuration)
+SHUTDOWN_TIMEOUT=${CONFIG#*=}
+if ! [[ $SHUTDOWN_TIMEOUT =~ ^[0-9]*$ ]]; then
+    echo "shutdown timeout is invalid"
+    exit 1
+fi
+is_shutting_down() {
+    is_shutting_down_ubuntu &> /dev/null || is_shutting_down_al1 &> /dev/null || is_shutting_down_al2 &> /dev/null
+}
+is_shutting_down_ubuntu() {
+    local TIMEOUT
+    TIMEOUT=$(busctl get-property org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager ScheduledShutdown)
+    if [ "$?" -ne "0" ]; then
+        return 1
+    fi
+    if [ "$(echo $TIMEOUT | awk "{print \$3}")" == "0" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+is_shutting_down_al1() {
+    pgrep shutdown
+}
+is_shutting_down_al2() {
+    local FILE
+    FILE=/run/systemd/shutdown/scheduled
+    if [[ -f "$FILE" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+is_vfs_connected() {
+    pgrep -f vfs-worker >/dev/null
+}
+
+is_vscode_connected() {
+    pgrep -u ec2-user -f .vscode-server/bin/ -a | grep -v -F 'shellIntegration-bash.sh' >/dev/null
+}
+
+is_intellij_connected() {
+    pgrep -u ec2-user -f JetBrains -a | grep -v -F 'remote-dev-server.sh' >/dev/null
+}
+
+if is_shutting_down; then
+    if [[ ! $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] || is_vfs_connected || is_vscode_connected || is_intellij_connected; then
+        sudo shutdown -c
+        echo > "/home/ec2-user/.c9/autoshutdown-timestamp"
+    else
+        TIMESTAMP=$(date +%s)
+        echo "$TIMESTAMP" > "/home/ec2-user/.c9/autoshutdown-timestamp"    
+    fi
+else
+    if [[ $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] && ! is_vfs_connected && ! is_vscode_connected && ! is_intellij_connected; then
+        sudo shutdown -h $SHUTDOWN_TIMEOUT
+    fi
+fi
